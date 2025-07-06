97d5e4dfdb6cebf0a3c504c2166dc7987a1b8e7f
Sumin <tnalsdldkr2@naver.com>
2025-07-07 03:34:21 +0900

    Initial commit

---
+++ src/app.tsx
@@ -0,0 +1,13 @@
+import { Hono } from "hono";
+import { federation } from "@fedify/fedify/x/hono";
+import { getLogger } from "@logtape/logtape";
+import fedi from "./federation.ts";
+
+const logger = getLogger("microblog");
+
+const app = new Hono();
+app.use(federation(fedi, () => undefined))
+
+app.get("/", (c) => c.text("Hello, Fedify!"));
+
+export default app;
---
+++ src/federation.ts
@@ -0,0 +1,20 @@
+import { createFederation, Person } from "@fedify/fedify";
+import { getLogger } from "@logtape/logtape";
+import { MemoryKvStore, InProcessMessageQueue } from "@fedify/fedify";
+
+const logger = getLogger("microblog");
+
+const federation = createFederation({
+  kv: new MemoryKvStore(),
+  queue: new InProcessMessageQueue(),
+});
+
+federation.setActorDispatcher("/users/{identifier}", async (ctx, identifier) => {
+  return new Person({
+    id: ctx.getActorUri(identifier),
+    preferredUsername: identifier,
+    name: identifier,
+  });
+});
+
+export default federation;
