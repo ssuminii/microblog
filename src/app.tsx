5396081a5427571f4461689687fb6e1d4d041cc4
Sumin <tnalsdldkr2@naver.com>
2025-07-16 02:30:30 +0900

    feat: add user setup handler and connect to SQLite using better-sqlite3

--- src/app.tsx
+++ src/app.tsx
@@ -3,16 +3,41 @@ import { federation } from "@fedify/fedify/x/hono";
 import { getLogger } from "@logtape/logtape";
 import fedi from "./federation.ts";
 import { Layout, SetupForm } from './views.tsx';
+import db from './db.ts';
+import type { User } from './schema.ts';
 
 const logger = getLogger("microblog");
 
 const app = new Hono();
 app.use(federation(fedi, () => undefined))
 
-app.get("/setup", (c) => c.html(
+app.get("/", (c) => c.text("Hello, Fedify!"));
+
+app.get("/setup", (c) => {
+  // 계정이 이미 있는지 검사
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  return c.html(
     <Layout>
       <SetupForm />
     </Layout>,
-  ),);
+  );
+});
+
+app.post("/setup", async (c) => {
+  // 계정이 이미 있는지 검사
+  const user = db.prepare<unknown[], User>("SELECT * FROM users LIMIT 1").get();
+  if (user != null) return c.redirect("/");
+
+  const form = await c.req.formData();
+  const username = form.get("username");
+  if (typeof username !== "string" || !username.match(/^[a-z0-9_-]{1,50}$/)) {
+    return c.redirect("/setup");
+  }
+  db.prepare("INSERT INTO users (username) VALUES (?)").run(username);
+  return c.redirect("/");
+});
+
 
 export default app;
